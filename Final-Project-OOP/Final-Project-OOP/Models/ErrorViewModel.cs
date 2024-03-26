b142077c36bb9116d36706cd6207241c4e63b84b
L9DJULO <98429398+L9DJULO@users.noreply.github.com>
2024-03-26 09:30:08 +0000

    first commit

---
+++ Final-Project-OOP/Final-Project-OOP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Final_Project_OOP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
