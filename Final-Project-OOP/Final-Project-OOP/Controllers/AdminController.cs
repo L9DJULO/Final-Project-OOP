662df24e4ddfc3356649e7405fc007dd6fba5cef
elbitor0 <123994826+elbitor0@users.noreply.github.com>
2024-04-09 07:46:11 +0100

    J'ai add pas mal de truc

--- Final-Project-OOP/Final-Project-OOP/Controllers/AdminController.cs
+++ Final-Project-OOP/Final-Project-OOP/Controllers/AdminController.cs
@@ -1,4 +1,8 @@
 ﻿using Final_Project_OOP.Models;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,8 +10,57 @@ namespace Final_Project_OOP.Controllers
 {
     public class AdminController : Controller
     {
-        // Action for displaying the admin dashboard
-        public IActionResult Dashboard()
+		// Action for displaying the admin dashboard
+		private readonly FirestoreDb _db;
+		private readonly FirebaseAuthClient _authClient;
+
+		public AdminController(IConfiguration configuration)
+		{
+
+			FirebaseAuthConfig _config = new FirebaseAuthConfig
+			{
+
+				ApiKey = configuration["Firebase:ApiKey"],
+				AuthDomain = configuration["Firebase:AuthDomain"],
+				Providers = new FirebaseAuthProvider[]
+				{
+                    // Add and configure individual providers
+                    //new GoogleProvider().AddScopes("email"),
+                    new EmailProvider()
+                    // ...
+                },
+				// WPF:
+				UserRepository = new FileUserRepository("FirebaseSample"), // persist data into %AppData%\FirebaseSample
+																		   // UWP:
+																		   //UserRepository = new StorageRepository() // persist data into ApplicationDataContainer
+			};
+
+			_authClient = new FirebaseAuthClient(_config);
+			_db = FirestoreDb.Create(configuration["Firebase:ProjectId"]);
+
+		}
+
+		public async Task<Admin> GetAdminFromId(string AdminId)
+		{
+			Query query = _db.Collection("users").WhereEqualTo("AdminId",AdminId);
+			QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
+
+
+
+			Admin admin = null;
+			foreach (DocumentSnapshot document in querySnapshot.Documents)
+			{
+				admin = document.ConvertTo<Admin>();
+			}
+
+			if (admin == null)
+			{
+				throw new Exception("Admin not found");
+			}
+
+			return admin;
+		}
+		public IActionResult Dashboard()
         {
             var courses = new List<Course>(); // Get courses from the database
 
@@ -25,26 +78,103 @@ namespace Final_Project_OOP.Controllers
             return View();
         }
 
-        // GET: AdminController/Create
-  
+		// GET: AdminController/Create
 
-        // POST: AdminController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
 
-        // GET: AdminController/Edit/5
-        public ActionResult Edit(int id)
+		public async Task<ActionResult> CreateAsync(Admin model)
+		{
+			TempData["Message"] = "";
+
+			try
+			{
+				var userCredential = await _authClient.CreateUserWithEmailAndPasswordAsync(model.email, model.Password, model.FirstName + " " + model.LastName);
+				// Handle successful SignUp 
+				//await _database.Collection("Users").Document(_authClient.User.Uid).SetAsync(model);
+				/*TODO: Add User To Database*/
+				CollectionReference collection = _db.Collection("users");
+				model.AdminId = userCredential.User.Uid;
+				model.Role = "Admin";
+				DocumentReference document = await collection.AddAsync(model);
+				return RedirectToAction(actionName: "Index", new { AdminId = model.AdminId });
+			}
+			catch (Exception ex)
+			{
+				// Handle SignUp failure
+				TempData["ErrorMessage"] = ex.Message;
+				return View(model);
+			}
+		}
+		// POST: AdminController/CreateCourse
+		[HttpPost]
+		public async Task<ActionResult> CreateCourse(Course model)
+		{
+			var course = new Course
+			{
+				Name = model.Name,
+				Code = model.Code,
+				Students = model.Students,
+				Description = model.Description,
+				Lecturer = model.Lecturer,
+				LecturerId = model.LecturerId,
+				StudentsId = model.StudentsId,
+				Assignments = model.Assignments,
+				AssignmentsId = model.AssignmentsId
+			};
+
+			await _db.Collection("courses").AddAsync(course);
+
+			return RedirectToAction("Index");
+		}
+
+		// POST: AdminController/CreateStudent
+		[HttpPost]
+		public async Task<ActionResult> CreateStudent(Student model)
+		{
+			var student = new Student
+			{
+				FirstName = model.FirstName,
+				Address = model.Address,
+				LastName = model.LastName,
+				CoursesId = model.CoursesId,
+				Assignments = model.Assignments,
+				AssignmentIds = model.AssignmentIds,
+				Grades = model.Grades,
+				email = model.email,
+				Password = model.Password,
+				Role = model.Role
+
+			};
+
+			await _db.Collection("Student").AddAsync(student);
+
+			return RedirectToAction("Index");
+		}
+
+		// POST: AdminController/CreateFacultyMember
+		[HttpPost]
+		public async Task<ActionResult> CreateFacultyMember(FacultyMember model)
+		{
+			var Lecturer = new FacultyMember
+			{
+				
+				FirstName = model.FirstName,
+				LastName= model.LastName,
+				Courses = model.Courses,
+				CoursesId= model.CoursesId,
+				email = model.email,
+				Password = model.Password,
+				Role = model.Role
+
+			};
+
+			await _db.Collection("Student").AddAsync(Lecturer);
+
+			return RedirectToAction("Index");
+		}
+
+
+		// GET: AdminController/Edit/5
+		public ActionResult Edit(int id)
         {
             return View();
         }
---
+++ Final-Project-OOP/Final-Project-OOP/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Google.Cloud.Firestore;
+
+namespace Final_Project_OOP.Models
+{
+	public class Admin : User
+
+	{
+		[FirestoreProperty] public string AdminId { get; set; }
+		[FirestoreProperty] public string FirstName { get; set; }
+		[FirestoreProperty] public string LastName { get; set; }
+	}
+}
